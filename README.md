41ab2f8b2eeca41117a53873cc1f1fe7b100ccbd
Mina Ayman <mtrmena176@gmail.com>
2026-02-25 05:59:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cancer_ai_detection
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cancer_ai_detection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cancer_ai_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
