283550efd4595aea0d109e85db255caf4c107ace
Mina Ayman <mtrmena176@gmail.com>
2026-02-26 21:26:22 +0200

    apply inter font

--- lib/theming/app_theme.dart
+++ lib/theming/app_theme.dart
@@ -1,11 +1,13 @@
 import 'package:cancer_ai_detection/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static const Color primaryColor = Color(0xFF13A4EC);
   static ThemeData get lightTheme {
     return ThemeData(
       useMaterial3: true,
+      textTheme: GoogleFonts.interTextTheme(),
       colorScheme: ColorScheme.fromSeed(
         seedColor: primaryColor,
         brightness: Brightness.light,
