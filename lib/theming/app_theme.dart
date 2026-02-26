dbcb517b550435e5bcf957ffa8dbac8173486c53
Mina Ayman <mtrmena176@gmail.com>
2026-02-26 17:30:00 +0200

    initial app theme

---
+++ lib/theming/app_theme.dart
@@ -0,0 +1,42 @@
+import 'package:cancer_ai_detection/constants.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF13A4EC);
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: primaryColor,
+        brightness: Brightness.light,
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        titleSpacing: Sizes.kHorizontalPadding,
+        toolbarHeight: 80,
+        actionsPadding: EdgeInsets.symmetric(
+          horizontal: Sizes.kHorizontalPadding,
+        ),
+      ),
+      listTileTheme: ListTileThemeData(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: Sizes.kHorizontalPadding,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+        selectedColor: primaryColor,
+        selectedTileColor: primaryColor.withOpacity(0.1),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryColor,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
