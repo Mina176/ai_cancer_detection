93ac3c5bd5b984cfa4afc55c22fb1f348c0dac38
Mina Ayman <mtrmena176@gmail.com>
2026-02-27 17:49:33 +0200

    init authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'package:cancer_ai_detection/features/upload_image/presentation/upload_screen_desktop.dart';
 import 'package:cancer_ai_detection/theming/app_theme.dart';
 import 'package:cancer_ai_detection/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:gp_backend_client/gp_backend_client.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
 
-void main() {
+late Client client;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const serverUrl = 'https://gp-api.lasheen.dev/';
+  client = Client(serverUrl)
+    ..connectivityMonitor = FlutterConnectivityMonitor()
+    ..authSessionManager = FlutterAuthSessionManager();
+  try {
+    await client.auth.initialize();
+  } catch (e) {
+    print('Failed to initialize Serverpod auth: $e');
+  }
   runApp(const MainApp());
 }
 
