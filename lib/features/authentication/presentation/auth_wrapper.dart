7d13547537a4b4488c9fafadb4adc4fd01eaf635
Mina Ayman <mtrmena176@gmail.com>
2026-02-28 16:53:37 +0200

    persistent auth state fix

--- lib/features/authentication/presentation/auth_wrapper.dart
+++ lib/features/authentication/presentation/auth_wrapper.dart
@@ -1,7 +1,5 @@
 import 'package:cancer_ai_detection/main.dart';
-import 'package:cancer_ai_detection/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
 class AuthWrapper extends StatelessWidget {
@@ -9,29 +7,18 @@ class AuthWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: client.authSessionManager.authInfoListenable,
-      builder: (context, authInfo, _) {
-        if (authInfo != null) {
-          Future.microtask(() => context.go(homeRoute));
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-        return Scaffold(
-          body: Center(
-            child: SignInWidget(
-              client: client,
-              onAuthenticated: () {},
-              onError: (error) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Error: $error')),
-                );
-              },
-            ),
-          ),
-        );
-      },
+    return Scaffold(
+      body: Center(
+        child: SignInWidget(
+          client: client,
+          onAuthenticated: () {},
+          onError: (error) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Error: $error')),
+            );
+          },
+        ),
+      ),
     );
   }
 }
