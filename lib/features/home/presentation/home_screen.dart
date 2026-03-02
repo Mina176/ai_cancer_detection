03da54e037d02fc00755c1c19fda8458b08da621
yousefIbrahem <ssnerd47@gmail>
2026-03-02 21:07:57 +0200

    gp

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:cancer_ai_detection/main.dart';
-import 'package:cancer_ai_detection/utils/app_router.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
