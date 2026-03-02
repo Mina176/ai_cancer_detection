872d9a3f6accb9bd35cea5f4b94b3edd9cfcb245
yousefIbrahem <ssnerd47@gmail>
2026-03-02 20:48:23 +0200

    form screens

---
+++ lib/features/user_role_selection/presentation/patient_form_screen.dart
@@ -0,0 +1,145 @@
+import 'package:cancer_ai_detection/main.dart';
+import 'package:flutter/material.dart';
+import 'package:gp_backend_client/gp_backend_client.dart';
+import 'package:intl/intl.dart';
+
+class PatientFormScreen extends StatefulWidget {
+  const PatientFormScreen({super.key});
+
+  @override
+  State<PatientFormScreen> createState() => _PatientFormScreenState();
+}
+
+class _PatientFormScreenState extends State<PatientFormScreen> {
+  final formKey = GlobalKey<FormState>();
+  DateTime selectedDate = DateTime.now();
+  Gender? selectedGender;
+  BloodType? selectedBloodType;
+  double? height;
+  double? weight;
+  String? smokingStatus;
+  int? smokingYears;
+  String? alcoholFrequency;
+  String? exerciseFrequency;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            spacing: 8,
+            children: [
+              ListTile(
+                dense: true,
+                onTap: () async {
+                  final pickedDate = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                    initialDate: DateTime.now(),
+                    builder: (context, child) => Padding(
+                      padding: EdgeInsetsGeometry.symmetric(
+                        vertical: 32,
+                        horizontal: 16,
+                      ),
+                      child: child,
+                    ),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      selectedDate = pickedDate;
+                    });
+                  }
+                },
+                leading: Icon(Icons.calendar_month),
+                title: const Text(
+                  "Date",
+                ),
+                subtitle: Text(
+                  DateFormat(' d/M/y').format(selectedDate),
+                ),
+              ),
+              Text('Gender'),
+              DropdownButtonFormField(
+                decoration: InputDecoration(hintText: 'Gender'),
+                items: [
+                  DropdownMenuItem(value: Gender.male, child: Text('Male')),
+                  DropdownMenuItem(value: Gender.female, child: Text('Female')),
+                ],
+                onChanged: (value) {
+                  selectedGender = value;
+                },
+              ),
+              DropdownButtonFormField<BloodType>(
+                decoration: const InputDecoration(hintText: 'Blood Type'),
+                items: BloodType.values.map((type) {
+                  return DropdownMenuItem<BloodType>(
+                    value: type,
+                    child: Text(type.name),
+                  );
+                }).toList(),
+                onChanged: (value) => setState(() {
+                  selectedBloodType = value;
+                }),
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Height'),
+                onChanged: (value) => setState(() {
+                  height = double.tryParse(value);
+                }),
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Weight'),
+                onChanged: (value) {
+                  weight = double.tryParse(value);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Smoking Status'),
+                onChanged: (value) => smokingStatus = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Smoking Years'),
+                onChanged: (value) => smokingYears = int.tryParse(value),
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Alcohol Frequnecy'),
+                onChanged: (value) => alcoholFrequency = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Exercise Frequnecy'),
+                onChanged: (value) => exerciseFrequency = value,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    client.patientProfileModelEdit.update(
+                      dob: selectedDate,
+                      gender: selectedGender,
+                      height: height,
+                      weight: weight,
+                      alcoholFreq: alcoholFrequency,
+                      bloodType: selectedBloodType,
+                    );
+                  }
+                },
+                child: Text('submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectDataOfBirth() async {
+    await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+  }
+}
