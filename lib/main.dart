49393d4a288d1f9225205c0e9f992c121023f22e
Mina176 <mtrmena176@gmail.com>
2026-04-11 08:49:02 +0200

    provide shared prefs

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:cancer_ai_detection/src/theming/app_theme.dart';
-import 'package:cancer_ai_detection/src/routing/app_router.dart';
+import 'package:cancer_ai_detection/src/routing/controller/app_router.dart';
+import 'package:cancer_ai_detection/src/utils/storage/shared_prefs/shared_prefs_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gp_backend_client/gp_backend_client.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 late Client client;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
   const serverUrl = 'https://gp-api.lasheen.dev/';
   client = Client(serverUrl)
     ..connectivityMonitor = FlutterConnectivityMonitor()
@@ -18,18 +21,25 @@ Future<void> main() async {
   } catch (e) {
     print('Error initializing authentication: $e');
   }
-  runApp(ProviderScope(child: const MainApp()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      routerConfig: router,
+      routerConfig: ref.watch(routerProvider),
     );
   }
 }
