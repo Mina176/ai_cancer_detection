0529169bf92810ce0e59b963a3d5d44964327252
Mina176 <mtrmena176@gmail.com>
2026-04-16 20:07:44 +0200

    patient details provider

---
+++ lib/src/features/doctor/patients/controller/patient_details_provider.dart
@@ -0,0 +1,10 @@
+import 'package:cancer_ai_detection/main.dart';
+import 'package:gp_backend_client/gp_backend_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'patient_details_provider.g.dart';
+
+@Riverpod()
+Future<PatientProfileModel> patientDetails(Ref ref, UuidValue patientId) async {
+  return client.patient.getPatient(patientId);
+}
