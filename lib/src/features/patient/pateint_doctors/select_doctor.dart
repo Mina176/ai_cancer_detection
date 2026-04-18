799c27f04e8b6e6a425e9969dae1fe8686a2312b
yousefIbrahem <ssnerd47@gmail>
2026-04-18 15:23:30 +0200

    create select doctor

---
+++ lib/src/features/patient/pateint_doctors/select_doctor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectDoctor extends StatefulWidget {
+  const SelectDoctor({super.key});
+
+  @override
+  State<SelectDoctor> createState() => _SelectDoctorState();
+}
+
+class _SelectDoctorState extends State<SelectDoctor> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
