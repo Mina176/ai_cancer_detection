88c2465525b8fb051651d8ca4e425adf7f4d3a61
Mina176 <mtrmena176@gmail.com>
2026-04-18 18:30:43 +0200

    lab profile provider

---
+++ lib/src/features/lab/controller/lab_profile_provider.dart
@@ -0,0 +1,10 @@
+import 'package:cancer_ai_detection/main.dart';
+import 'package:gp_backend_client/gp_backend_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'lab_profile_provider.g.dart';
+
+@Riverpod()
+Future<LabProfileModel> labProfile(Ref ref) async {
+  return await client.labProfile.getOrCreate();
+}
