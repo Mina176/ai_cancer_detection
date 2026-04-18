e55df512ae04ac5208025273e686c97eb7377868
Mina176 <mtrmena176@gmail.com>
2026-04-18 15:34:06 +0200

    generated code

--- lib/src/features/doctor/patients/controller/patient_details_provider.g.dart
+++ lib/src/features/doctor/patients/controller/patient_details_provider.g.dart
@@ -85,3 +85,74 @@ final class PatientDetailsFamily extends $Family
   @override
   String toString() => r'patientDetailsProvider';
 }
+
+@ProviderFor(generatePatientAnalysis)
+final generatePatientAnalysisProvider = GeneratePatientAnalysisFamily._();
+
+final class GeneratePatientAnalysisProvider
+    extends $FunctionalProvider<AsyncValue<String>, String, FutureOr<String>>
+    with $FutureModifier<String>, $FutureProvider<String> {
+  GeneratePatientAnalysisProvider._({
+    required GeneratePatientAnalysisFamily super.from,
+    required UuidValue super.argument,
+  }) : super(
+         retry: null,
+         name: r'generatePatientAnalysisProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$generatePatientAnalysisHash();
+
+  @override
+  String toString() {
+    return r'generatePatientAnalysisProvider'
+        ''
+        '($argument)';
+  }
+
+  @$internal
+  @override
+  $FutureProviderElement<String> $createElement($ProviderPointer pointer) =>
+      $FutureProviderElement(pointer);
+
+  @override
+  FutureOr<String> create(Ref ref) {
+    final argument = this.argument as UuidValue;
+    return generatePatientAnalysis(ref, argument);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is GeneratePatientAnalysisProvider &&
+        other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$generatePatientAnalysisHash() =>
+    r'db87a08fc1daa57cbd7dea3ac1d1aef13765ba27';
+
+final class GeneratePatientAnalysisFamily extends $Family
+    with $FunctionalFamilyOverride<FutureOr<String>, UuidValue> {
+  GeneratePatientAnalysisFamily._()
+    : super(
+        retry: null,
+        name: r'generatePatientAnalysisProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  GeneratePatientAnalysisProvider call(UuidValue patientId) =>
+      GeneratePatientAnalysisProvider._(argument: patientId, from: this);
+
+  @override
+  String toString() => r'generatePatientAnalysisProvider';
+}
